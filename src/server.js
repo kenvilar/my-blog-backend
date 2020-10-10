94747509ec0d85113ae9d6d8821df20b8dc656bd
Ken Vilar <kenvilar@gmail.com>
2020-10-10 18:56:02 +0800

    Sample get and post

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/hello', function(req, res) {
   console.log('ken hello');
   res.send('Hello');
+
+});
+
+app.post('/hello', function(req, res) {
+  res.send(`Hello ${req.body.name}!`);
 });
 
 app.listen(8000, function() {
