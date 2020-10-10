823f26e8aee27d9aba0188c565882d8d4c3891c9
Ken Vilar <kenvilar@gmail.com>
2020-10-10 20:43:19 +0800

    Route params

--- src/server.js
+++ src/server.js
@@ -10,6 +10,10 @@ app.get('/hello', (req, res) => {
   res.send('Hello');
 });
 
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
+
 app.post('/hello', (req, res) => {
   res.send(`Hello ${req.body.name}!`);
 });
